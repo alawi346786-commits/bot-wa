efe82538a6e9f1c440ec903509d994bb4825b494
alawi346786-commits <alawi346786@gmail.com>
2026-07-22 21:41:37 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
-
-const client = new Client({
-    authStrategy: new LocalAuth(),
-    puppeteer: {
-        args: [
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-            '--disable-dev-shm-usage',
-            '--disable-accelerated-2d-canvas',
-            '--no-first-run',
-            '--no-zygote',
-            '--disable-gpu'
-        ]
-    }
-});
-
 const qrcodeTerminal = require('qrcode-terminal'); // Untuk login bot
 const QRCode = require('qrcode'); // Untuk fitur pembuat QR Code
 const { createCanvas } = require('canvas');
